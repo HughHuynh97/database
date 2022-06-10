13b8f47d40e9978e4ec15e2fd0e5f5915b83a5ef
trivip002 <40769628+trivip002@users.noreply.github.com>
2022-06-11 02:44:52 +0700

    #Init

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,27 @@
 app="database"
+app2="crawl-service"
 username="trivip002"
-docker network prune
+
+if [[ -n "$( docker network -q spring-net )" ]]; then
+  echo "MYSQL Container exists"
+else
+  docker create -p 3307:3306 --name mysqldb -e MYSQL_ROOT_PASSWORD=tripro123  -e MYSQL_DATABASE=dev mysql:8.0.29
+fi
+docker network rm spring-net
 docker network create spring-net
-docker network connect spring-net mysqldb
-echo Build Image
 docker stop "$app" && docker rm -f "$app"
 docker pull "$username"/"$app":latest
-echo Create Container
+docker stop "$app2" && docker rm -f "$app2"
+docker pull "$username"/"$app2":latest
 docker create -p 8081:8081 --name "$app" --net spring-net -e MYSQL_HOST=mysqldb -e MYSQL_USERNAME=root -e MYSQL_PASSWORD=tripro123 -e MYSQL_PORT=3306 -e MYSQL_DATABASE=dev "$username"/"$app"
+docker create -p 8080:8080 --name "$app2" --net spring-net -e MYSQL_HOST=mysqldb -e MYSQL_USERNAME=root -e MYSQL_PASSWORD=tripro123 -e MYSQL_PORT=3306 -e MYSQL_DATABASE=dev "$username"/"$app2"
+docker network connect spring-net mysqldb
+docker container start mysqldb
+sleep 6
+echo Begin Start Module "$app"
+docker container start "$app"
+sleep 6
+echo Begin Start Module "$app2"
+docker container start "$app2"
+
+echo Deploy Done !
