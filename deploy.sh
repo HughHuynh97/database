623fdad4e673df9cae5d114c2bc62566287e16c3
trivip002 <40769628+trivip002@users.noreply.github.com>
2022-06-11 00:59:49 +0700

    #Init

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 app="database"
 username="trivip002"
-echo Build Image
+echo Pull Image
 docker stop "$app" && docker rm -f "$app"
 docker pull "$username"/"$app":latest
+echo Create Container
 docker create -p 8081:8081 --name "$app" --net spring-net -e MYSQL_HOST=mysqldb -e MYSQL_USERNAME=root -e MYSQL_PASSWORD=tripro123 -e MYSQL_PORT=3306 -e MYSQL_DATABASE=dev "$username"/"$app"
