dd60acbff27710b1482153f794813f4c448c42eb
HughHuynh97 <106435137+HughHuynh97@users.noreply.github.com>
2022-06-10 17:14:04 +0700

    Update DatabaseApplication.java

--- src/main/java/com/product/database/DatabaseApplication.java
+++ src/main/java/com/product/database/DatabaseApplication.java
@@ -2,7 +2,7 @@ package com.product.database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+sadsad
 @SpringBootApplication
 public class DatabaseApplication {
 
