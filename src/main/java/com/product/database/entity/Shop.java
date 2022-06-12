ada064acb3de0f8b207cc33a165ae47db4b0f549
trivip002 <40769628+trivip002@users.noreply.github.com>
2022-06-13 00:00:41 +0700

    #Init

--- src/main/java/com/product/database/entity/Shop.java
+++ src/main/java/com/product/database/entity/Shop.java
@@ -1,22 +1,27 @@
 package com.product.database.entity;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "shop")
-public class Shop {
-
+public class Shop implements Serializable {
     @Id
     @Column(name = "shop_id")
     private Long shopId;
+    @Id
+    @Column(name = "provider")
+    private String provider;
+    @Column(name = "user_id")
+    private Long userId;
     @Column(name = "name")
     private String name;
     @Column(name = "username")
     private String username;
     @Column(name = "avatar")
     private String avatar;
-    @Column(name = "cover_image")
-    private String coverImage;
+    @Column(name = "background")
+    private String background;
     @Column(name = "shop_location")
     private String shopLocation;
     @Column(name = "rating_star")
@@ -27,10 +32,10 @@ public class Shop {
     private String ratingNormal;
     @Column(name = "rating_bad")
     private String ratingBad;
-    @Column(name = "item_count")
-    private Long itemCount;
-    @Column(name = "follower_count")
-    private Long followerCount;
+    @Column(name = "total_product")
+    private Long totalProduct;
+    @Column(name = "follower")
+    private Long follower;
     @Column(name = "description", columnDefinition = "TEXT NOT NULL")
     private String description;
     @Column(name = "country")
